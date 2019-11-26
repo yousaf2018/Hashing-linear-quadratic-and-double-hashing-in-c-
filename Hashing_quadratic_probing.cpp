a5f69a84589adf94dd2cc0abde577bf4a49d6d63
yousaf2018 <mahmoodyousaf975974@gmail.com>
2019-11-26 15:36:41 +0500

    Start to practice

---
+++ Hashing_quadratic_probing.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+
+using namespace std;
+
+class Hashing
+{
+    public:
+    Hashing();
+    int Hashing_table[7];
+    int hashing_function(int);
+    int collision_function_quadratic_probing(int);
+    void add_value_in_hash_table(int);
+    void display();
+    int search_in_hash_table(int);
+    int delete_in_hash_table(int);
+    void is_full();
+    void is_empty();
+};
+Hashing::Hashing()
+{
+    for(int a=0;a<7;a++)
+    {
+        Hashing_table[a]=-1;
+    }
+}
+int Hashing::hashing_function(int data)
+{
+            return data%7;
+}
+int Hashing::collision_function_quadratic_probing(int data)
+{
+ int counter=1;
+ while(true)
+ {
+  if(Hashing_table[(hashing_function(data)+counter*counter)%7]==-1)
+  {
+      return (hashing_function(data)+counter*counter)%7;
+  }
+   counter++;
+ }
+}
+void Hashing::add_value_in_hash_table(int input)
+{
+    int check;
+    check=hashing_function(input);
+    if(Hashing_table[check]==-1)
+    {
+        Hashing_table[check]=input;
+        return;
+    }
+    else
+    {
+    check=collision_function_quadratic_probing(input);
+    {
+        Hashing_table[check]=input;
+    }
+    }
+}
+void Hashing::display()
+{
+    int a;
+    for(a=0;a<7;a++)
+    cout << Hashing_table[a] << endl;
+}
+int Hashing::search_in_hash_table(int input)
+{
+    int check;
+    check=hashing_function(input);
+    if(Hashing_table[check]==input)
+    {
+        return 1;
+    }
+    else
+  {
+    int counter=1;
+    while(true)
+    {
+    if(Hashing_table[(hashing_function(input)+counter*counter)%7]==input)
+    {
+    return 1;
+    }
+    counter++;
+    if(counter>7)
+    {
+        return -1;
+    }
+    }
+  }
+}
+int Hashing::delete_in_hash_table(int input)
+{
+    int check;
+    check=hashing_function(input);
+    if(Hashing_table[check]==input)
+    {
+        Hashing_table[check]=-1;
+        return 1;
+    }
+    else
+  {
+    int counter=1;
+    while(true)
+    {
+    if(Hashing_table[(hashing_function(input)+counter*counter)%7]==input)
+    {
+        Hashing_table[(hashing_function(input)+counter*counter)%7]=-1;
+        return 1;
+    }
+    counter++;
+    if(counter>7)
+    {
+        return -1;
+    }
+    }
+  }
+}
+void Hashing::is_empty()
+{
+    int a;
+    for(a=0;a<7;a++)
+    {
+        if(Hashing_table[a]!=-1)
+        {
+            cout << "Hash table is not empty\n";
+            return;
+        }
+    }
+    cout << "Hash table is empty\n";
+    return;
+}
+void Hashing::is_full()
+{
+    int a;
+    for(a=0;a<7;a++)
+    {
+        if(Hashing_table[a]==-1)
+        {
+            cout << "Hash table is not full\n";
+            return;
+        }
+    }
+    cout << "Hash table is full\n";
+    return;
+}
+int main()
+{
+ Hashing object;
+ int check;
+        while(true)
+        {
+                int option,input;
+                cout << "****************************************\n";
+                cout << "Enter 1 for insert value in Hash table\n";
+                cout << "Enter 2 for display\n";
+                cout << "Enter 3 for search value in Hash table\n";
+                cout << "Enter 4 for remove value in Hash\n";
+                cout << "Enter 5 for check is full or not\n";
+                cout << "Enter 6 for check is empty or not\n";
+                cout << "****************************************\n";
+                cin >> option;
+                if(option==1)
+                {
+                        cout << "Enter value to insert in Hash table\n";
+                        cin >> input;
+                        object.add_value_in_hash_table(input);
+                }
+                else if(option==2)
+                {
+                        object.display();
+                }
+                else if(option==3)
+                {
+                        cout << "Enter value you want to search\n";
+                        cin >> input;
+                        check=object.search_in_hash_table(input);
+                        if(check==-1)
+                        {
+                                cout << "No match value is found in Hash table\n";
+                        }
+                        else if(check==1)
+                        {
+                                cout << "Yes value is matched in Hash table\n";
+                        }
+                }
+                else if(option==4)
+                {
+                        cout << "Enter value you want to remove\n";
+                        cin >> input;
+                        check=object.delete_in_hash_table(input);
+                        if(check==-1)
+                        {
+                                cout << "No match value is found in Hash table\n";
+                        }
+                        else if(check==1)
+                        {
+                                cout << "Yes value is deleted in Hash table\n";
+                        }
+                }
+                else if(option==5)
+                {
+                        object.is_full();
+                }
+                else if(option==6)
+                {
+                        object.is_empty();
+                }
+
+        }
+}
+
